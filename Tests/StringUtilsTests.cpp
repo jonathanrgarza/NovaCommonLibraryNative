31ba9efb7f9852abc0e02a1f292315089bb7b4d1
Jonathan Garza <83787061+jonathanrgarza@users.noreply.github.com>
2022-09-06 02:17:28 -0500

    Add unit tests for strTrimLeft, strTrimRight, strTrim, strIsNullTerminated, strEnsureNullTerminated

--- Tests/StringUtilsTests.cpp
+++ Tests/StringUtilsTests.cpp
@@ -8,22 +8,281 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "modernize-avoid-c-arrays"
 
+#include "TestUtils.h"
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/catch_section_info.hpp>
 
 #include "StringUtils.h"
 
-TEST_CASE( "Null-Terminated String Results In True", "[strIsNullTerminated]" )
+TEST_CASE( "Null-Terminated string results in true", "[strIsNullTerminated]" )
 {
+	//Arrange
     constexpr char text[] = "hello";
+	//Act
+	//Assert
     REQUIRE(Ncl::strIsNullTerminated(text, sizeof(text)) == true);
 }
 
-TEST_CASE( "Non-Null-Terminated String Results In False", "[strIsNullTerminated]" )
+TEST_CASE( "Non-Null-Terminated string results in false", "[strIsNullTerminated]" )
 {
+	//Arrange
     char text[] = "hello";
     text[5] = ' ';
+	//Act
+	//Assert
     REQUIRE(Ncl::strIsNullTerminated(text, sizeof(text)) == false);
+}
+
+TEST_CASE( "Null-Terminated string results in no throw", "[strEnsureNullTerminated]" )
+{
+	//Arrange
+	constexpr char text[] = "hello";
+	//Act
+	//Assert
+	REQUIRE_NOTHROW(Ncl::strEnsureNullTerminated(text, sizeof(text)));
+}
+
+TEST_CASE( "Non-Null-Terminated string results in throw", "[strEnsureNullTerminated]" )
+{
+	//Arrange
+	char text[] = "hello";
+	text[5] = ' ';
+	//Act
+	//Assert
+	REQUIRE_THROWS_AS(Ncl::strEnsureNullTerminated(text, sizeof(text)), std::invalid_argument);
+}
+
+TEST_CASE( "String with beginning spaces has it removed", "[strTrimLeft]" )
+{
+	//Arrange
+	constexpr char expected[] = "test ";
+	char actual[] = "    test ";
+
+	//Act
+	Ncl::strTrimLeft(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with beginning tabs has it removed", "[strTrimLeft]" )
+{
+	//Arrange
+	constexpr char expected[] = "test ";
+	char actual[] = "\t\t\ttest ";
+
+	//Act
+	Ncl::strTrimLeft(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with beginning newlines has it removed", "[strTrimLeft]" )
+{
+	//Arrange
+	constexpr char expected[] = "test ";
+	char actual[] = "\n\n\ntest ";
+
+	//Act
+	Ncl::strTrimLeft(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with all whitespace returns empty string", "[strTrimLeft]" )
+{
+	//Arrange
+	constexpr char expected[] = "";
+	char actual[] = "    \t\n    ";
+
+	//Act
+	Ncl::strTrimLeft(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with text content returns the same", "[strTrimLeft]" )
+{
+	//Arrange
+	constexpr char expected[] = "test ";
+	char actual[] = "test ";
+
+	//Act
+	Ncl::strTrimLeft(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "Null string returns the same", "[strTrimLeft]" )
+{
+	//Arrange
+	constexpr char* expected = nullptr;
+	char* actual = nullptr;
+
+	//Act
+	Ncl::strTrimLeft(actual, 0);
+
+	//Assert
+	REQUIRE(expected == actual);
+}
+
+TEST_CASE( "String with ending spaces has it removed", "[strTrimRight]" )
+{
+	//Arrange
+	constexpr char expected[] = " test";
+	char actual[] = " test    ";
+
+	//Act
+	Ncl::strTrimRight(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with ending tabs has it removed", "[strTrimRight]" )
+{
+	//Arrange
+	constexpr char expected[] = " test";
+	char actual[] = " test\t\t\t\t";
+
+	//Act
+	Ncl::strTrimRight(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with ending newlines has it removed", "[strTrimRight]" )
+{
+	//Arrange
+	constexpr char expected[] = " test";
+	char actual[] = " test\n\n\n";
+
+	//Act
+	Ncl::strTrimRight(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with all whitespace returns empty string", "[strTrimRight]" )
+{
+	//Arrange
+	constexpr char expected[] = "";
+	char actual[] = "        ";
+
+	//Act
+	Ncl::strTrimRight(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with text content returns the same", "[strTrimRight]" )
+{
+	//Arrange
+	constexpr char expected[] = " test";
+	char actual[] = " test";
+
+	//Act
+	Ncl::strTrimRight(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "Null string returns the same", "[strTrimRight]" )
+{
+	//Arrange
+	constexpr char* expected = nullptr;
+	char* actual = nullptr;
+
+	//Act
+	Ncl::strTrimRight(actual, 0);
+
+	//Assert
+	REQUIRE(expected == actual);
+}
+
+TEST_CASE( "String with beginning and ending spaces has it removed", "[strTrim]" )
+{
+	//Arrange
+	constexpr char expected[] = "test test";
+	char actual[] = "  test test   ";
+
+	//Act
+	Ncl::strTrim(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with beginning and ending tabs has it removed", "[strTrim]" )
+{
+	//Arrange
+	constexpr char expected[] = "test test";
+	char actual[] = "\t\t test test\t\t\t\t";
+
+	//Act
+	Ncl::strTrim(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with beginning and ending newlines has it removed", "[strTrim]" )
+{
+	//Arrange
+	constexpr char expected[] = "test test";
+	char actual[] = "\n\n test test\n\n\n";
+
+	//Act
+	Ncl::strTrim(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with all whitespace returns empty string", "[strTrim]" )
+{
+	//Arrange
+	constexpr char expected[] = "";
+	char actual[] = "        ";
+
+	//Act
+	Ncl::strTrim(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "String with text content returns the same", "[strTrim]" )
+{
+	//Arrange
+	constexpr char expected[] = "test test";
+	char actual[] = "test test";
+
+	//Act
+	Ncl::strTrim(actual, sizeof(actual));
+
+	//Assert
+	STR_EQUAL_REQUIRE(expected, actual);
+}
+
+TEST_CASE( "Null string returns the same", "[strTrim]" )
+{
+	//Arrange
+	constexpr char* expected = nullptr;
+	char* actual = nullptr;
+
+	//Act
+	Ncl::strTrim(actual, 0);
+
+	//Assert
+	REQUIRE(expected == actual);
 }
 
 #pragma clang diagnostic pop
---
+++ Tests/TestUtils.h
@@ -0,0 +1,37 @@
+//
+// Created by Jonathan on 9/6/2022.
+//
+
+#ifndef NOVACOMMONLIBRARYNATIVE_TESTUTILS_H
+#define NOVACOMMONLIBRARYNATIVE_TESTUTILS_H
+
+#include <string>
+
+template<std::size_t N, std::size_t O>
+auto strEquals(const char(&str)[N], const char(&other)[O]) -> bool // NOLINT(modernize-avoid-c-arrays)
+{
+	size_t minSize = std::min(N, O);
+	return strncmp(str, other, minSize) == 0;
+};
+
+#define STR_EQUAL_CHECK(expected_, actual_) \
+  INFO( "expected = " << '"' << (expected_) << '"');  \
+  INFO( "actual   = " << '"' << (actual_)   << '"');  \
+  CHECK(strEquals(actual_, expected_));
+
+#define STR_EQUAL_REQUIRE(expected_, actual_) \
+  INFO( "expected = " << '"' << (expected_) << '"');  \
+  INFO( "actual   = " << '"' << (actual_)   << '"');  \
+  REQUIRE(strEquals(expected_, actual_));
+
+#define STR_NOT_EQUAL_CHECK(expected_, actual_) \
+  INFO( "expected = " << '"' << (expected_) << '"');  \
+  INFO( "actual   = " << '"' << (actual_)   << '"');  \
+  CHECK(!strEquals(actual_, expected_));
+
+#define STR_NOT_EQUAL_REQUIRE(expected_, actual_) \
+  INFO( "expected = " << '"' << (expected_) << '"');  \
+  INFO( "actual   = " << '"' << (actual_)   << '"');  \
+  REQUIRE(!strEquals(expected_, actual_));
+
+#endif //NOVACOMMONLIBRARYNATIVE_TESTUTILS_H
